1368627c5b907fe3838081fff599a10a1bd6e47e
Androff <rageclock23@gmail.com>
2025-03-16 17:02:23 +0100

    feat: goo

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -88,8 +88,8 @@ function getNextFriday(data) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -103,8 +103,10 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const date1 = new Date(dateStart).getTime();
+  const date2 = new Date(dateEnd).getTime();
+  return Math.ceil((date2 - date1) / 1000 / 3600 / 24) + 1;
 }
 
 /**
